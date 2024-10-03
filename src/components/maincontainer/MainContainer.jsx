4af718688ab413b636f3d766a068d6e31652c641
221244_Tebogo <110398577+221244-Tebogo@users.noreply.github.com>
2024-10-03 17:14:10 +0200

    Add files via upload

--- src/components/maincontainer/MainContainer.jsx
+++ src/components/maincontainer/MainContainer.jsx
@@ -6,7 +6,10 @@ function MainContainer({ children }) {
     <div className="mainContainer">
       <div className="Statistics"></div>
       <div className="centre">{children}</div>
+<<<<<<< HEAD
       {/* <div className="item item-3">Item 3</div> */}
+=======
+>>>>>>> ea5d899a4 (Your commit message)
     </div>
   );
 }
